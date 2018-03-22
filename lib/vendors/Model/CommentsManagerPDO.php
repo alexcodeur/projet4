34197199f107c5dc939a009443ecb89dd8546907
alexcodeur <33685231+alexcodeur@users.noreply.github.com>
2018-03-22 09:56:10 +0100

    Update CommentsManagerPDO.php

--- lib/vendors/Model/CommentsManagerPDO.php
+++ lib/vendors/Model/CommentsManagerPDO.php
@@ -7,9 +7,9 @@ class CommentsManagerPDO extends CommentsManager
 {
   protected function add(Comments $comment)
   {
-    $q = $this->dao->prepare('INSERT INTO Comments SET news = :news, name = :name, comment = :comment, date_add_comment = NOW()');
+    $q = $this->dao->prepare('INSERT INTO comments SET chapter = :chapter, name = :name, comment = :comment, date_add_comment = NOW()');
 
-    $q->bindValue(':news', $comment->news(), \PDO::PARAM_INT);
+    $q->bindValue(':chapter', $comment->chapter(), \PDO::PARAM_INT);
     $q->bindValue(':name', $comment->name(), \PDO::PARAM_STR);
     $q->bindValue(':comment', $comment->comment(), \PDO::PARAM_STR);
 
@@ -18,15 +18,15 @@ class CommentsManagerPDO extends CommentsManager
     $comment->setId($this->dao->lastInsertId());
   }
 
-  public function getListOf($news)
+  public function getListOf($chapter)
   {
-    if (!ctype_digit($news))
+    if (!ctype_digit($chapter))
     {
-      throw new \InvalidArgumentException('L\'identifiant de la news passé doit être un nombre entier valide');
+      throw new \InvalidArgumentException('L\'identifiant du chapitre passé doit être un nombre entier valide');
     }
 
-    $q = $this->dao->prepare('SELECT id, news, name, comment, DATE_FORMAT(date_add_comment, \'%d-%m-%Y à %Hh:%imin:%ss\') AS date_add_comment_fr FROM Comments WHERE news = :news');
-    $q->bindValue(':news', $news, \PDO::PARAM_INT);
+    $q = $this->dao->prepare('SELECT id, chapter, name, comment, signalComment, DATE_FORMAT(date_add_comment, \'%d-%m-%Y à %Hh:%imin\') AS date_add_comment_fr, signalComment FROM comments WHERE chapter = :chapter');
+    $q->bindValue(':chapter', $chapter, \PDO::PARAM_INT);
     $q->execute();
 
     $comments = $q->fetchAll();
@@ -34,24 +34,22 @@ class CommentsManagerPDO extends CommentsManager
     return $comments;
   }
 
-  protected function modify(Comment $comment)
+  public function validate($comment)
   {
-    $q = $this->dao->prepare('UPDATE comments SET auteur = :auteur, contenu = :contenu WHERE id = :id');
+    $q = $this->dao->prepare('UPDATE comments SET signalComment = 1 WHERE id = :id');
 
-    $q->bindValue(':auteur', $comment->auteur());
-    $q->bindValue(':contenu', $comment->contenu());
-    $q->bindValue(':id', $comment->id(), \PDO::PARAM_INT);
+    $q->bindValue(':id', $comment, \PDO::PARAM_INT);
 
     $q->execute();
   }
 
   public function get($id)
   {
-    $q = $this->dao->prepare('SELECT id, news, auteur, contenu FROM comments WHERE id = :id');
+    $q = $this->dao->prepare('SELECT id, chapter, name, comment, signalComment FROM comments WHERE id = :id');
     $q->bindValue(':id', (int) $id, \PDO::PARAM_INT);
     $q->execute();
 
-    $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Comment');
+    $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'vendors\Entity\Comments');
     return $q->fetch();
   }
 
@@ -60,8 +58,36 @@ class CommentsManagerPDO extends CommentsManager
     $this->dao->exec('DELETE FROM comments WHERE id = '.(int) $id);
   }
 
-  public function deleteFromNews($news)
+  public function deleteFromChapter($chapter)
   {
-    $this->dao->exec('DELETE FROM comments WHERE news = '.(int) $news);
+    $this->dao->exec('DELETE FROM comments WHERE chapter = '.(int) $chapter);
+  }
+
+  public function signalComment($commentSignal)
+  {
+      $q = $this->dao->prepare('UPDATE comments SET signalComment = 0 WHERE id = :id');
+      $q->bindValue(':id', (int) $commentSignal, \PDO::PARAM_INT);
+      $q->execute();
+  }
+
+  public function commentPosted($id)
+  {
+      $q = $this->dao->prepare('SELECT signalComment FROM comments WHERE id = :id');
+      $q->bindValue(':id', $id, \PDO::PARAM_INT);
+      $q->execute();
+
+      return $q->fetch();
+  }
+
+  public function commentSignaled()
+  {
+      $q = $this->dao->query('SELECT chapter, signalComment FROM comments WHERE signalComment = FALSE');
+
+      return $q->fetchAll(\PDO::FETCH_CLASS, Comments::class);
+  }
+
+  public function count()
+  {
+      return $this->dao->query('SELECT COUNT(*) FROM comments')->fetchColumn();
   }
 }

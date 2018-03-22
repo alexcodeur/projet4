22f0584e857c771f0b6fa3fcbba6c8b63bdd1b9a
alexcodeur <alexandre.dijos.ad@gmail.com>
2018-03-22 10:38:40 +0100

    index connexion

---
+++ App/Backend/Modules/Connexion/View/index.php
@@ -0,0 +1,12 @@
+<h2 style="text-align: center;">Connexion administrateur</h2>
+
+<form action="" method="post" style="margin-top: 50px; margin-bottom: 169px; text-align: center;">
+  <label>Pseudo</label><br />
+  <input class="input-connexion" type="text" name="login" /><br />
+  <br />
+  <label>Mot de passe</label><br />
+  <input class="input-connexion" type="password" name="password" /><br /><br />
+
+  <input type="submit" value="Connexion" class="btn btn-primary" />
+</form>
+

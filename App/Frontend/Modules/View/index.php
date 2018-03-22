2ce4beb9b73f22905440310bdaef76b2c438996e
alexcodeur <33685231+alexcodeur@users.noreply.github.com>
2018-03-22 10:53:26 +0100

    Delete index.php

--- App/Frontend/Modules/View/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach ($listeNews as $news)
-{
-?>
-  <h2><a href="news-<?= $news['id'] ?>.html"><?= htmlspecialchars($news['title']) ?></a></h2>
-  <p><?= nl2br(htmlspecialchars($news['article'])) ?></p>
-<?php
-}
-?>
